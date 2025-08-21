3b135453346c2f177872ff8079490afd47339264
luukdg <luukdegraaf1993@gmail.com>
2025-08-21 21:54:44 +0200

    Enhance todo functionality: integrate ticket creation, update form handling, and localize UI text

--- src/dom/buttonInteractions.js
+++ src/dom/buttonInteractions.js
@@ -1,10 +1,14 @@
 // <--- Add todo Button ---> //
+import createTicket from "../components/createTicket";
+
+const newTicket = new createTicket();
 
 export default class DomFunctions {
   constructor() {
     this.elements = {
       addTodo: document.getElementById("add-todo-button"),
       submit: document.getElementById("submit"),
+      toDoForm: document.getElementsByClassName("add-book-popup"),
     };
   }
 
@@ -14,9 +18,16 @@ export default class DomFunctions {
     });
   }
 
+  // Submit a todo ticket
   addSubmit() {
-    this.elements.submit?.addEventListener("click", () => {
-      alert("added");
-    });
+    this.elements.submit?.addEventListener("click", () => {});
+    const toDoInput = this.toDoForm;
+
+    const title = toDoInput.querySelector("title").value.trim();
+    const description = toDoInput.querySelector("title").value.trim();
+    const priority = toDoInput.querySelector("priority");
+    const date = toDoInput.querySelector("date");
+
+    newTicket(title, description, priority, date);
   }
 }
